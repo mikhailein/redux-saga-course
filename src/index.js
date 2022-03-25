0a34034b93283b4e13961c433849e9f631a9fee4
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-03-25 14:04:37 +0000

    initial

--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './pages/App';
-import { store } from './redux';
-import { Router } from 'react-router-dom'
-import { Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Blog from './pages/Blog';
-import NotFound from './pages/NotFound';
-import { history } from './redux/reducers';
-import { ConnectedRouter } from 'connected-react-router';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route path='/' exact>
-            <App />
-          </Route>
-          <Route path='/blog' exact>
-            <Blog />
-          </Route>
-          <Route path='*'>
-            <NotFound />
-          </Route>
-        </Switch>
-      </ConnectedRouter>
-    </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
